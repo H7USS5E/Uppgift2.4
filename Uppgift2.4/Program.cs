7d3d0378ed2064b43d7f07c536fa7bacf66f3038
Admin <Admin@TEB123013>
2023-09-29 14:42:03 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in lönen på 3 av dina anställda");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+            double summa = (x + y + z) / 3;
+            Console.WriteLine("Medellönen för din personal är " + summa);
+        }
+    }
+}
+
